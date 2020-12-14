2def240ef01042daa07405659293d2e715ac431c
David Issa Mattos <issamattos.david@gmail.com>
2020-12-14 07:49:50 +0100

    Adding publication related functions for ploting and creating tables

---
+++ R/bpc_publication.R
@@ -0,0 +1,203 @@
+#' Publication-ready table for the parameter estimates
+#'
+#' @param bpc_object a bpc object
+#' @param params a vector with the parameters to be in the table. If NULL them all will be present
+#' @param format A character string. same formats utilized in the knitr::kable function
+#' * 'latex': output in latex format
+#' * 'simple': appropriated for the console
+#' * 'pipe': Pandoc's pipe tables
+#' * 'html': for html formats
+#' * 'rst'
+#' @param digits number of digits in the table
+#' @param caption a string containing the caption of the table
+#' @param HPDI a boolean if the intervals should be credible (F) or HPD intervals (T)
+#' @return a formatted table
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' m<-bpc(data = tennis_agresti,
+#' player0 = 'player0',
+#' player1 = 'player1',
+#' result_column = 'y',
+#' model_type = 'bt',
+#' solve_ties = 'none')
+#' t<-get_parameter_table(m)
+#' print(t)
+#' }
+get_parameters_table <-
+  function(bpc_object,
+           params=NULL,
+           format = 'latex',
+           digits = 3,
+           caption = 'Parameters estimates',
+           HPDI = T) {
+    if (class(bpc_object) != 'bpc')
+      stop('Error! The object is not of bpc class')
+    t <- get_parameters(bpc_object, params=params, HPDI = HPDI)
+    out <-
+      knitr::kable(t,
+                   format = format,
+                   digits = digits,
+                   caption = caption)
+    return(out)
+  }
+
+
+#' Publication-ready table for the probabilities
+#'
+#' @param bpc_object a bpc object
+#' @param format A character string. same formats utilized in the knitr::kable function
+#' * 'latex': output in latex format
+#' * 'simple': appropriated for the console
+#' * 'pipe': Pandoc's pipe tables
+#' * 'html': for html formats
+#' * 'rst'
+#'  Possible values are latex, html, p, simple (Pandoc's simple tables), and rst.
+#' @param digits number of digits in the table
+#' @param caption a string containing the caption of the table
+#' @return a formatted table
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' m<-bpc(data = tennis_agresti,
+#' player0 = 'player0',
+#' player1 = 'player1',
+#' result_column = 'y',
+#' model_type = 'bt',
+#' solve_ties = 'none')
+#' t<-get_probabilities_table(m)
+#' print(t)
+#' }
+get_probabilities_table <-
+  function(bpc_object,
+           format = 'latex',
+           digits = 3,
+           caption = 'Estimated posterior probabilites') {
+    if (class(bpc_object) != 'bpc')
+      stop('Error! The object is not of bpc class')
+    t <- get_probabilities_df(bpc_object)
+    out <-
+      knitr::kable(t,
+                   format = format,
+                   digits = digits,
+                   caption = caption)
+    return(out)
+  }
+
+
+#' Publication-ready table for the rank table
+#'
+#' @param bpc_object a bpc object
+#' @param format A character string. same formats utilized in the knitr::kable function
+#' * 'latex': output in latex format
+#' * 'simple': appropriated for the console
+#' * 'pipe': Pandoc's pipe tables
+#' * 'html': for html formats
+#' * 'rst'
+#' * 'simple' appropriated for the console
+#' @param digits number of digits in the table
+#' @param caption a string containing the caption of the table
+#' @param n number of times we are sampling the posterior
+#' @return a formatted table
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' m<-bpc(data = tennis_agresti,
+#' player0 = 'player0',
+#' player1 = 'player1',
+#' result_column = 'y',
+#' model_type = 'bt',
+#' solve_ties = 'none')
+#' t<-get_rank_of_players_table(m)
+#' print(t)
+#' }
+get_rank_of_players_table <-
+  function(bpc_object,
+           format = 'latex',
+           digits = 3,
+           caption = 'Estimated posterior ranks',
+           n = 1000) {
+    if (class(bpc_object) != 'bpc')
+      stop('Error! The object is not of bpc class')
+    t <- get_rank_of_players_df(bpc_object , n = n)
+    out <-
+      knitr::kable(t,
+                   format = format,
+                   digits = digits,
+                   caption = caption)
+    return(out)
+  }
+
+
+#' Return a publication-ready plot for the parameters estimates based on the HPD interval
+#' The returned plot is a caterpillar type of plot
+#' @param bpc_object a bpc fitted object
+#' @param HPDI use HPD (TRUE) or credible intervals (FALSE) for the plots
+#' @param params a vector of string for of the parameters to be plotted
+#' @param title the title of the plot
+#' @param subtitle optional subtitle for the plot
+#' @param xaxis title of the x axis
+#' @param yaxis title of the y axis
+#' @param rotate_x_labels should the labels be shown horizontally (default, FALSE) or vertically (TRUE)
+#' @param APA should the graphic be formatted in APA style (default TRUE)
+#' @return a ggplot2 caterpillar plot
+#' @importFrom rlang .data
+#' @export
+#'
+#' @examples
+#' #' \donttest{
+#' m<-bpc(data = tennis_agresti,
+#' player0 = 'player0',
+#' player1 = 'player1',
+#' result_column = 'y',
+#' model_type = 'bt',
+#' solve_ties = 'none')
+#' p<-get_parameters_plot(m)
+#' p
+#' }
+get_parameters_plot <-
+  function(bpc_object,
+           HPDI = T,
+           params = c('lambda'),
+           title = 'Parameter estimates',
+           subtitle = NULL,
+           xaxis = 'Parameter',
+           yaxis = 'Value',
+           rotate_x_labels = FALSE,
+           APA=TRUE) {
+    df <- get_parameters(bpc_object,
+                         HPDI = HPDI,
+                         n_eff = F,
+                         Rhat = F)
+
+
+    colnames(df) <- c('Parameter', 'Mean', 'Lower', 'Higher')
+
+    df <- df %>%
+      dplyr::filter(stringr::str_detect(.data$Parameter, paste(params, collapse = "|"))) %>%
+      dplyr::arrange(.data$Mean)
+
+    param_order <- as.array(df$Parameter)
+
+    out <- ggplot2::ggplot(df, ggplot2::aes(x = factor(.data$Parameter, levels = param_order))) + #to order the variables properly we need to convert to factor and use the levels. It is not enough the order of the dataframe
+      ggplot2::geom_pointrange(ggplot2::aes(
+        ymin = .data$Lower,
+        ymax = .data$Higher,
+        y = .data$Mean
+      )) +
+      ggplot2::labs(
+        y = yaxis,
+        x = xaxis,
+        title = title,
+        subtitle = subtitle
+      )
+    if(APA)
+      out <- out + jtools::theme_apa()
+    if(rotate_x_labels)
+      out <- out+ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) #to rotate we need to adjust the anchor position as well
+
+    return(out)
+  }
